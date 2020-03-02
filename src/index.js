23049f243adf771c20e0b3417cdfa697475e6423
Sergey <bashkin2013@mail.ru>
2020-03-02 19:55:28 +0400

    fear: add function for task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseWords = new Array();
+    for (let i = 0; i < expr.length; i += 10) {
+        let word = expr[i] + expr[i + 1] + expr[i + 2] + expr[i + 3] + expr[i + 4] + expr[i + 5] + expr[i + 6] +
+            expr[i + 7] + expr[i + 8] + expr[i + 9]
+        let morse = '';
+        for (let i = 0; i < word.length; i += 2) {
+            if (Number(word[i] + word[i + 1]) == 10) {
+                morse += '.'
+            } else {
+                if (Number(word[i] + word[i + 1]) == 11) {
+                    morse += '-';
+                }
+            }
+        }
+        morseWords.push(morse);
+    }
+    let string = '';
+    morseWords.map(el => {
+        if (el == '') {
+            string += ' ';
+        } else {
+            string += MORSE_TABLE[el];
+        };
+    })
+    return string;
 }
 
 module.exports = {
